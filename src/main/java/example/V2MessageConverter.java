32bc91db31a3dc5451f8044fe10c3fecb292ecc4
hiroykub <hiroykub@amazon.co.jp>
2021-01-28 12:36:52 +0000

    hapi imported

--- src/main/java/example/V2MessageConverter.java
+++ src/main/java/example/V2MessageConverter.java
@@ -4,9 +4,9 @@ import ca.uhn.hl7v2.DefaultHapiContext;
 import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.HapiContext;
 import ca.uhn.hl7v2.model.Message;
-import ca.uhn.hl7v2.model.v22.datatype.PN;
-import ca.uhn.hl7v2.model.v22.message.ADT_A01;
-import ca.uhn.hl7v2.model.v22.segment.MSH;
+import ca.uhn.hl7v2.model.v251.datatype.XPN;
+import ca.uhn.hl7v2.model.v251.message.ADT_A01;
+import ca.uhn.hl7v2.model.v251.segment.MSH;
 import ca.uhn.hl7v2.parser.EncodingNotSupportedException;
 import ca.uhn.hl7v2.parser.Parser;
 
@@ -18,6 +18,6 @@ public class V2MessageConverter{
         
     }
     public Patient getPatient(){
-        
+        return new Patient();
     }
 }
