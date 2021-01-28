3d9f1d274a43812a7bd2c570215205645490c971
MasahiroImai <masazdream@gmail.com>
2021-01-28 13:06:05 +0000

    adding post

--- src/main/java/example/ApiGatewayClient.java
+++ src/main/java/example/ApiGatewayClient.java
@@ -46,4 +46,38 @@ public class ApiGatewayClient {
             throw e;
         }
     }
+    
+    public HttpResponse<String> post(String endpoint, String token, String payload) throws Exception {
+        try {
+            logger.info(System.getenv(ENV_API_KEY));
+            
+            HttpRequest req = HttpRequest.newBuilder(new URI(endpoint))
+            .setHeader(
+                "x-api-key", System.getenv(ENV_API_KEY)
+            // ).setHeader(
+            //     "Content-Type", "application/json"
+            // )
+            ).setHeader(
+                "Authorization", token
+            ).POST(
+                HttpRequest.BodyPublishers.ofString(payload)    
+            ).build();
+            
+            HttpClient client = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_1_1)
+            .followRedirects(HttpClient.Redirect.NORMAL)
+            .connectTimeout(Duration.ofSeconds(10))
+            .build();
+            
+            HttpResponse<String> res = client.send(req, HttpResponse.BodyHandlers.ofString());
+            int statusCode = res.statusCode();
+            String body = res.body();
+            System.out.println("statusCode: " + statusCode);
+            System.out.println("body" + body);
+            
+            return res;
+        } catch (Exception e) {
+            throw e;
+        }
+    }
 }
