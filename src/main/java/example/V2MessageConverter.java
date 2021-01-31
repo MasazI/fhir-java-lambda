02bdeba982dd0368bd8e9d675ce95e7a8397d4fb
hiroykub <hiroykub@amazon.co.jp>
2021-01-31 12:57:46 +0000

    observation convert finished

--- src/main/java/example/V2MessageConverter.java
+++ src/main/java/example/V2MessageConverter.java
@@ -4,11 +4,13 @@ import ca.uhn.hl7v2.DefaultHapiContext;
 import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.HapiContext;
 import ca.uhn.hl7v2.model.Message;
+import ca.uhn.hl7v2.model.Varies;
 import ca.uhn.hl7v2.model.v251.datatype.CX;
 import ca.uhn.hl7v2.model.v251.datatype.XPN;
 import ca.uhn.hl7v2.model.v251.datatype.TS;
 import ca.uhn.hl7v2.model.v251.datatype.IS;
 import ca.uhn.hl7v2.model.v251.datatype.FN;
+import ca.uhn.hl7v2.model.v251.datatype.NM;
 import ca.uhn.hl7v2.model.v251.message.OUL_R22;
 import ca.uhn.hl7v2.model.v251.group.OUL_R22_PATIENT;
 import ca.uhn.hl7v2.model.v251.group.OUL_R22_SPECIMEN;
@@ -23,6 +25,9 @@ import example.pojo.patient.Patient;
 import example.pojo.patient.Identifier;
 import example.pojo.patient.Name;
 import example.pojo.observation.Observation;
+import example.pojo.observation.ValueQuantity;
+import example.pojo.observation.Code;
+import example.pojo.observation.Coding_;
 
 import java.io.InputStream;
 import java.io.BufferedInputStream;
@@ -30,6 +35,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Date;
 import java.text.SimpleDateFormat;
+import java.lang.Double;
 
 public class V2MessageConverter{
   private Patient patient;
@@ -55,11 +61,6 @@ public class V2MessageConverter{
         TS dateTimeOfBirth = pid.getPid7_DateTimeOfBirth();
         IS administrativeSex = pid.getPid8_AdministrativeSex();
         System.out.println("patientName=" + patientName);
-        
-        //Patient ID
-        //Identifier[] patientIDList = new Identifier[1];
-        //patientIDList[0].setValue(patientID.getIDNumber().toString());
-        //patient.setIdentifier(patientIDList);
         
         List<Identifier> patientIDList = new ArrayList<Identifier>();
         Identifier identifier = new Identifier();
@@ -106,10 +107,40 @@ public class V2MessageConverter{
         for (int i = 0; i < oulMsg.getSPECIMENReps(); i++){
           Observation observation = new Observation();
           OBX obx = oulMsg.getSPECIMEN(i).getOBX();
-          //OBX-2,3,5,6,12,14
+          //OBX-2 skipped
           
+          //OBX-3
+          Coding_ coding = new Coding_();
+          coding.setCode(obx.getObx3_ObservationIdentifier().getCe1_Identifier().getValue());
+          coding.setDisplay(obx.getObx3_ObservationIdentifier().getCe2_Text().getValue());
+          List codings = new ArrayList<Coding_>();
+          codings.add(coding);
+          Code code = new Code();
+          code.setCoding(codings);
+          observation.setCode(code);
+          //OBX-5 & OBX-6
+          ValueQuantity valueQuantity = new ValueQuantity();
+          Varies customValue = obx.getObx5_ObservationValue(1);
+          NM numericValue = (NM)customValue.getData();
+          Double value = new Double(numericValue.getValue());
+          valueQuantity.setValue(value);
+          valueQuantity.setUnit(obx.getObx6_Units().getCe2_Text().getValue());
+          observation.setValueQuantity(valueQuantity);
           
+          //OBX-11
+          switch(obx.getObx11_ObservationResultStatus().getValue()){
+            case "F":
+            observation.setStatus("final");
+            break;
+          default:
+            observation.setStatus("unknown");
+            break;            
+          }
           
+          //OBX-14
+          Date observationDate = obx.getObx14_DateTimeOfTheObservation().getTime().getValueAsDate();
+          String strObservationDate = new SimpleDateFormat("yyyyMMddhhmmss").format(observationDate);
+          observation.setEffectiveDateTime(strObservationDate);
           
           observationList.add(observation);
         }
